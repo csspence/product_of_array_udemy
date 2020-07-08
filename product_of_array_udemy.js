3e58f9d94a9b78e4c83e634e3644326a446ed71b
Crew Spence <csspence123@gmail.com>
2020-07-08 06:16:37 -0500

    Created function stub for toy problem.

---
+++ product_of_array_udemy.js
@@ -0,0 +1,13 @@
+/*
+JavaScript Algorithms and Data Structures Masterclass
+
+productOfArray
+Write a function called productOfArray which takes in an array of numbers and returns the product of them all.
+*/
+
+const productOfArray = (arr) => {
+  
+}
+
+// productOfArray([1,2,3]) // 6
+// productOfArray([1,2,3,10]) // 60
